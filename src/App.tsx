5ce84c09828abf3a28a20e366cccc9986ec53768
booi-dev <booimangang@gmail.com>
2023-08-06 22:16:24 +0530

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-red-500 h-4 w-full">
+        <h2 className="text-2xl">hi, vite. Nice to see u again</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
